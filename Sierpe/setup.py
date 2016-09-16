75131169aa69ebca836648db75791c789df01367
jmbenlloch <info@jmbenlloch.net>
2016-09-16 12:51:34 +0200

    Cython SensorResponseC

---
+++ Sierpe/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+
+import numpy
+
+setup(
+    #ext_modules = cythonize("SensorsResponseC.pyx", "BRLC.pyx"),
+    ext_modules = cythonize("*.pyx"),
+    include_dirs = [numpy.get_include()]
+)
